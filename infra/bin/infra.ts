8ce5885aef64bb83ea110027372b9140bb7acd79
omid eidivandi <eidivandi@live.com>
2024-03-30 00:25:42 +0100

    Update infra.ts

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,11 +5,9 @@ import { InfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
 
-cdk.Tags.of(app).add('costCenter', "aviv");
 cdk.Tags.of(app).add('application', "testapp");
-cdk.Tags.of(app).add('team', "arsene");
+cdk.Tags.of(app).add('team', "myteam");
 cdk.Tags.of(app).add('environment', "dev");
 cdk.Tags.of(app).add('dataClassification', 'internal');
-cdk.Tags.of(app).add('taggingVersion', '1.0.0');
 
 new InfraStack(app, 'InfraStack');
